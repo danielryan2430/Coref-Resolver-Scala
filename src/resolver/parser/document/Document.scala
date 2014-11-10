2961866ef4fd9213291edd5d2b2ae650b5424e9b
Daniel Imberman <danielryan2430@gmail.com>
2014-11-10 09:24:55 -0800

    minor clean up

--- src/resolver/parser/document/Document.scala
+++ src/resolver/parser/document/Document.scala
@@ -3,6 +3,4 @@ package resolver.parser.document
 /**
  * Created by dimberman on 11/9/14.
  */
-class Document(f:Seq[Classifier]) {
-    var features: Seq[Classifier] = f
-}
+class Document(features:Seq[Classifier])
